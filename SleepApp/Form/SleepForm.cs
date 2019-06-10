933f6effe82e06bce29b3c05ee8a552ae2bc214a
Tomomasa Abe <miyabi.ver39@gmail.com>
2019-06-10 23:50:27 +0900

    future 7

--- SleepApp/Form/SleepForm.cs
+++ SleepApp/Form/SleepForm.cs
@@ -11,125 +11,142 @@ using System.Windows.Forms;
 
 namespace SleepApp
 {
-	public partial class SleepForm : Form
-	{
-		private SleepController sleepController;
-		private BackgroundWorker backgroundWorker;
+    public partial class SleepForm : Form
+    {
+        private SleepController sleepController;
+        private BackgroundWorker backgroundWorker;
 
         private bool isForceDown = false;
         private bool isForceVisible = false;
 
-		/// <summary>
-		/// コンストラクタ
-		/// </summary>
-		public SleepForm()
-		{
-			InitializeComponent();
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public SleepForm()
+        {
+            InitializeComponent();
 
             ToolStripMenuItem menuItemVisible = new ToolStripMenuItem();
             menuItemVisible.Text = "&表示";
-            menuItemVisible.Click += new EventHandler(ForceVisible_Click);
+            menuItemVisible.Click += new EventHandler(ToolStripMenuItem_ForceVisible);
             contextMenuStrip.Items.Add(menuItemVisible);
 
             ToolStripMenuItem menuItemExit = new ToolStripMenuItem();
             menuItemExit.Text = "&終了";
-            menuItemExit.Click += new EventHandler(Close_Click);
+            menuItemExit.Click += new EventHandler(ToolStripMenuItem_Close);
             contextMenuStrip.Items.Add(menuItemExit);
 
             notifyIcon.ContextMenuStrip = contextMenuStrip;
 
             // ワーカースレッド
             backgroundWorker = new BackgroundWorker();
-			backgroundWorker.WorkerSupportsCancellation = true;
-			backgroundWorker.WorkerReportsProgress = true;
+            backgroundWorker.WorkerSupportsCancellation = true;
+            backgroundWorker.WorkerReportsProgress = true;
 
-			backgroundWorker.DoWork += new DoWorkEventHandler(backgroundWorker_doWork);
-			backgroundWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(backgroundWorker_RunWorkerCompleted);
+            backgroundWorker.DoWork += new DoWorkEventHandler(backgroundWorker_doWork);
+            backgroundWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(backgroundWorker_RunWorkerCompleted);
 
-			sleepController = new SleepController(100, Program.SleepCheckTime);
-			sleepController.DoStartAsync();
+            sleepController = new SleepController(100, Program.SleepCheckTime);
+            sleepController.DoStartAsync();
 
-			backgroundWorker.RunWorkerAsync();
+            backgroundWorker.RunWorkerAsync();
 
-			SystemEvents.PowerModeChanged += (sender, e) =>
-			{
-				switch (e.Mode)
-				{
-					case PowerModes.Suspend:
-						// スリープ直前
-						sleepController.IsCancel = true;
-						break;
-					case PowerModes.Resume:
-						sleepController.DoStartAsync();
-						// 復帰直後
-						break;
-					case PowerModes.StatusChange:
-						// バッテリーや電源に関する通知があった
-						break;
-				}
-			};
-		}
+            SystemEvents.PowerModeChanged += (sender, e) =>
+            {
+                switch (e.Mode)
+                {
+                    case PowerModes.Suspend:
+                        // スリープ直前
+                        sleepController.IsCancel = true;
+                        break;
+                    case PowerModes.Resume:
+                        sleepController.DoStartAsync();
+                        // 復帰直後
+                        break;
+                    case PowerModes.StatusChange:
+                        // バッテリーや電源に関する通知があった
+                        break;
+                }
+            };
+        }
 
-		/// <summary>
-		///  ロードイベント
-		/// </summary>
-		/// <param name="sender"></param>
-		/// <param name="e"></param>
-		private void SleepForm_Load(object sender, EventArgs e)
-		{
-			this.Visible = false;
-		}
+        /// <summary>
+        /// ロードイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SleepForm_Load(object sender, EventArgs e)
+        {
+            this.Visible = false;
+        }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="sender"></param>
-		/// <param name="e"></param>
-		private void backgroundWorker_doWork(object sender, DoWorkEventArgs e)
-		{
-			try
-			{
-				BackgroundWorker worker = sender as BackgroundWorker;
+        /// <summary>
+        /// スリープまでの残り時間表示
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void backgroundWorker_doWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                BackgroundWorker worker = sender as BackgroundWorker;
 
-				while (true)
-				{
-					// 0.5秒スリープ
-					System.Threading.Thread.Sleep(500);
-					
-					// キャンセルの場合
-					if (worker.CancellationPending)
-					{
-						e.Cancel = true;
-						break;
-					}
+                while (true)
+                {
+                    // 0.5秒スリープ
+                    System.Threading.Thread.Sleep(500);
 
-                    // プログレスバー適用前にチェック
-                    int val = progressBar.Maximum - sleepController.SleepElapsedTime;
-
-                    if (progressBar.Maximum < val)
+                    // キャンセルの場合
+                    if (worker.CancellationPending)
                     {
-                        val = progressBar.Maximum;
-                    }
-                    else if (val < progressBar.Minimum)
-                    {
-                        val = progressBar.Minimum;
+                        e.Cancel = true;
+                        break;
                     }
 
-                    // 残り時間の表示
-                    string displaysleepElapsedTime = (sleepController.SleepElapsedTime > 9999) ? "+9999" : sleepController.SleepElapsedTime.ToString();
+                    // プログレスバー適用前に値が範囲内に当てはまるようにする
+                    int progressBarVal = progressBar.Maximum - sleepController.SleepElapsedTime;
+
+                    // プログレスバー以上ならば、プログレスバーの最大値を指定
+                    if (progressBar.Maximum < progressBarVal)
+                    {
+                        progressBarVal = progressBar.Maximum;
+                    }
+                    // プログレスバー以下ならば、プログレスバーの最低値を指定
+                    else if (progressBarVal < progressBar.Minimum)
+                    {
+                        progressBarVal = progressBar.Minimum;
+                    }
+
+                    // 残り時間
+                    string displaysleepElapsedTime;
+
+                    // 残り時間が9999秒以上ならば
+                    if (sleepController.SleepElapsedTime > 9999)
+                    {
+                        displaysleepElapsedTime = "+9999";
+                    }
+                    // それ以外
+                    else {
+                        displaysleepElapsedTime = sleepController.SleepElapsedTime.ToString();
+                    }
+
+                    // 画面の残り時間を反映
                     Invoke((MethodInvoker)delegate
                     {
                         SleepTimeLabel.Text = displaysleepElapsedTime;
                     });
+
+                    // 通知アイコンの残り時間を反映
                     notifyIcon.Text = "スリープまで残り" + displaysleepElapsedTime + "秒";
 
                     // 画面操作
                     Invoke((MethodInvoker)delegate
                     {
                         // プログレスバーの値変更
-                        progressBar.Value = val;
+                        progressBar.Value = progressBarVal;
                     });
 
+                    // 強制表示状態ならば
                     if (isForceVisible)
                     {
                         Invoke((MethodInvoker)delegate
@@ -145,15 +162,15 @@ namespace SleepApp
 
                     // 表示範囲内ならば画面を表示
                     if (Program.SleepVisibleTime >= sleepController.SleepElapsedTime)
-					{
+                    {
                         // 画面操作
                         Invoke((MethodInvoker)delegate
                         {
-							// 表示状態に変更
-							if (!this.Visible)
-							{
-								this.Visible = true;
-							}
+                            // 表示状態に変更
+                            if (!this.Visible)
+                            {
+                                this.Visible = true;
+                            }
 
                             // 最前面に表示
                             if (!this.TopMost)
@@ -162,59 +179,67 @@ namespace SleepApp
                             }
                         });
                     }
-					else
-					{
-						// 画面操作
-						Invoke((MethodInvoker)delegate
-						{
-							if (this.Visible)
-							{
-								this.Visible = false;
-							}
-						});
-						continue;
-					}
-				}
-			}
-			catch (Exception ex)
-			{
-				MessageBox.Show(ex.Message);
+                    // 表示範囲外ならば
+                    else
+                    {
+                        // 画面操作
+                        Invoke((MethodInvoker)delegate
+                        {
+                            // 非表示状態に変更
+                            if (this.Visible)
+                            {
+                                this.Visible = false;
+                            }
+                        });
+                        continue;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
                 Program.logger.Error("障害：" + ex.Message);
                 // 何もしない
             }
-		}
+        }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="sender"></param>
-		/// <param name="e"></param>
-		private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        /// <summary>
+        /// ワーカー終了時の動作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
+            // 再起動させる
             Program.logger.Info("情報：ReStart");
             sleepController.IsCancel = true;
             sleepController.DoStartAsync();
             backgroundWorker.RunWorkerAsync();
         }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="sender"></param>
-		/// <param name="e"></param>
-		private void SleepForm_FormClosing(object sender, FormClosingEventArgs e)
-		{
+        /// <summary>
+        /// フォームを閉じたときのイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SleepForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
             if (!isForceDown)
             {
                 e.Cancel = true;
                 this.Visible = false;
                 isForceVisible = false;
             }
-		}
+        }
 
-		private void SleepForm_Shown(object sender, EventArgs e)
-		{
-			this.Visible = false;
+        /// <summary>
+        /// フォームが表示された時のイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SleepForm_Shown(object sender, EventArgs e)
+        {
+            this.Visible = false;
             isForceVisible = false;
 
             Invoke((MethodInvoker)delegate
@@ -224,17 +249,32 @@ namespace SleepApp
             });
         }
 
-        private void Close_Click(object sender, EventArgs e)
+        /// <summary>
+        /// メニュー 閉じるイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ToolStripMenuItem_Close(object sender, EventArgs e)
         {
             isForceDown = true;
             Application.Exit();
         }
 
-        private void ForceVisible_Click(object sender, EventArgs e)
+        /// <summary>
+        /// メニュー 表示イベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ToolStripMenuItem_ForceVisible(object sender, EventArgs e)
         {
             isForceVisible = true;
         }
 
+        /// <summary>
+        /// 通知アイコンクリックイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void notifyIcon_Click(object sender, EventArgs e)
         {
             // 表示する必要はないので無効
